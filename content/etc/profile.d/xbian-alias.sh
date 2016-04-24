9b2ce67b4833a87b8bcd8aacdfd5c65efeda5b26
mkreisl <manfred.kreisl@nexgo.de>
2016-04-24 14:38:17 +0200

    Adding command 'kodi' to start/stop Kodi

--- content/etc/profile.d/xbian-alias.sh
+++ content/etc/profile.d/xbian-alias.sh
@@ -5,3 +5,4 @@ alias xl="tailf /home/xbian/.$nm/temp/$nm.log"
 alias xll="less /home/xbian/.$nm/temp/$nm.log"
 alias xlll="while :; do tailf /home/xbian/.$nm/temp/$nm.log; done"
 
+kodi () { if [ "$1" = start -o "$1" = stop -o "$1" = status ]; then sudo $1 xbmc; else echo "usage:"; echo "  kodi start|stop|status"; fi; }
