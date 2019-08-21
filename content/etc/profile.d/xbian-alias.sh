e6d28d8601f90f370872dc5ccef939571a9da287
mkreisl <manfred.kreisl@nexgo.de>
2019-08-21 16:07:00 +0200

    Fixes for Debian Buster: - tailf not longer available, so change tailf to tail -f and - adding alias for tailf

--- content/etc/profile.d/xbian-alias.sh
+++ content/etc/profile.d/xbian-alias.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+command -v tailf >/dev/null || alias tailf="tail -f"
+
 [ -x /usr/local/lib/kodi/kodi.bin ] && nm=kodi || nm=xbmc
-alias xl="tailf /home/xbian/.$nm/temp/$nm.log"
+alias xl="tail -f /home/xbian/.$nm/temp/$nm.log"
 alias xll="less /home/xbian/.$nm/temp/$nm.log"
-alias xlll="while :; do tailf /home/xbian/.$nm/temp/$nm.log; done"
+alias xlll="while :; do tail -f /home/xbian/.$nm/temp/$nm.log; done"
 
 kodi () { if [ "$1" = start -o "$1" = stop -o "$1" = status ]; then sudo $1 xbmc; else echo "usage:"; echo "  kodi start|stop|status"; fi; }
